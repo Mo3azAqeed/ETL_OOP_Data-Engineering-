8125839825ca36f952381ba3b822e0d2da86259e
Moaz El Aqeed <moazaqeed0100@gmail.com>
2024-09-17 10:03:07 +0300

    This is the last updates

--- Task_2.py
+++ Task_2.py
@@ -2,12 +2,12 @@ import os
 import pandas as pd 
 from abc import ABC, abstractmethod
 import requests 
-
+import json
 
 class DataSource(ABC):
-    def __init__(self, file_path, schema):
+    def __init__(self, connection_details, schema):
         self.schema = schema
-        self.file_path = file_path
+        self.connection_details = connection_details
 
     @abstractmethod
     def load_data(self):
@@ -28,6 +28,12 @@ class DataSource(ABC):
     def Transform_Data(self):
         pass
 
+class SchemaMismatchError(Exception):
+    raise ValueError ("The Schema is unmatched")
+
+class FileFormatError(Exception):
+    raise TypeError("The File Has A Wrong Format")
+
 class CSVSource(DataSource):
 
     def __init__(self, file_path, schema):
@@ -44,9 +50,7 @@ class CSVSource(DataSource):
             else:
                 raise ValueError(f"Schema mismatch: file has {len(pd.read_csv(self.file_path))} columns, but expected {int(self.schema)}.")
         else:
-            raise Exception("The file is not in CSV format")
-
-
+            raise SchemaMismatchError("The file is not in CSV format")
 
     def Transform_Data(self):
         print(f"Transforming data from file: {self.file_path}")
@@ -60,4 +64,23 @@ class CSVSource(DataSource):
         return (self.df.head())
 
 
+class JSONSource(DataSource):
+    def __init__(self,file_path,schema):
+        super().__init__(file_path,schema)
+        self.df=self.load_data()
+    def load_data(self,file_path):
+        if self.DataSource_type== ".json" :
+            print(f"Loading Json file from: {self.file_path}")
+            if len(list(self.file_path.keys())) == int(self.schema):
+                 return pd.read_json(self.file_path)
+            else: 
+                raise SchemaMismatchError ("The file is not in Json format")
+        else:
+            raise FileFormatError ("The file is not in Json format")
+
+class SQLSource(DataSource):
+    def __init__(data_base_key,schema):
+        super().__init__(file_path,schema) 
+
+    
 
